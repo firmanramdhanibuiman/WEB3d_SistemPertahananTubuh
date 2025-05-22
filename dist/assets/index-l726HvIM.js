ee3cc6257c147b1034499ede6d0d8051f9d9ea95
Firman Ramdhani <firmanramdhanibuiman@gmail.com>
2025-05-22 20:54:12 +0700

    fix: set correct base path for Netlify

---
+++ dist/assets/index-l726HvIM.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f2df02a0366f0e68d9175e379f2f357dfd38797e7f227d3160df118e50116ac5
+size 1508031
--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5c8a520451fa96bd701299990e857db2219c1d1c0820176edd5fec5da90cfc73
-size 88
+oid sha256:9ba1074fc320f19d529992b73b539f9b7ad72189c57de13e269cc8c7828145ff
+size 266
