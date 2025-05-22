e4b2a5b5f66b87ca17f1ad51a049051296a95fb8
Firman Ramdhani <firmanramdhanibuiman@gmail.com>
2025-05-22 20:28:51 +0700

    fix: set base path for Netlify

--- components/Materi1.jsx
+++ components/Materi1.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:27b627bcf08795ce6f632ef6b01186a2715174c3e3a87e83f48bd989828bb091
-size 4537
+oid sha256:abcca08a020c236bab046e3ce895a564dfb52f19944e9a79279f98042017c0f4
+size 4703
---
+++ vite.config.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:52a9fc87a9d58c0837fe34cc4ecf775e1183f7f5410e902a385b44ef5958ed8e
+size 89
