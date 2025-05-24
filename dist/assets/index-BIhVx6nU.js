d7ceb8ec88e4d0d46e95c93b98f65ecae6378ef4
Firman Ramdhani <firmanramdhanibuiman@gmail.com>
2025-05-24 20:03:37 +0700

    fix: set correct base path for Netlify

--- dist/assets/index-BIhVx6nU.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:b32f8f6ddb75c3f3cef2a586989548e76eb40d182b0d4b673c55badf887d0168
-size 1514168
---
+++ dist/assets/index-BZKoIadM.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9980616a738290dc7e974f6f09e02f0346cf9cd39906257a2cd6eec819b68a00
+size 1514207
