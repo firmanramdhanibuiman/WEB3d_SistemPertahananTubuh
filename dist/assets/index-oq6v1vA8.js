6ca79a0485294e220e066749c7eac562e9646b35
Firman Ramdhani <firmanramdhanibuiman@gmail.com>
2025-05-28 08:59:56 +0700

    fix: set to final correct base path for Netlify

---
+++ dist/assets/index-oq6v1vA8.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c2199d58e4b3fc9e08895bd0ab1bb78ec24a0dcec5b9801b9fcbed5223036390
+size 1517324
--- src/components/VirusModel.jsx
+++ src/components/VirusModel.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1fc011fbcda360bcb4280f9949a8d63353fa06423f7dab7abbc27a14e9cff755
-size 2010
+oid sha256:1614be51121275b5f6f7c15f7b024fb0409295f85be8a2ea4fa3a03ad2110331
+size 1930
